48f5c8811c1b575eaf4ecac2b6dc77233e734688
Zhaoxian Hu <huzhx@uci.edu>
2021-05-12 10:12:15 -0700

    move source code under src folder

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import express from 'express';
 const app = express();
 const PORT = 8000;
 app.get('/', (req, res) => res.send('Express + Typescript'));
+
+app.post('/meetings', (req, res) => {
+  return res.send('create a meeting')
+})
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
