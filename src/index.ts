a4b2b8376abc62b2a66078e5e6e49896ad5e9dca
Zhaoxian Hu <huzhx@uci.edu>
2021-05-19 10:55:48 -0700

    update the controller

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ import { MeetingParticipantsInterface } from './interfaces/meeting-participants-
 import { MeetingParticipants } from './value-objects/meeting-participants';
 import { MeetingTimeInterface } from './interfaces/meeting-time-interface';
 import { MeetingTime } from './value-objects/meeting-time';
+import { MeetingProviderForVSee } from './meeting-providers/meeting-provider-for-vsee';
+import { MeetingScheduleService } from './servives/meeting-schedule-service';
+import { MeetingRepositoryUsingMock } from './repositories/meeting-repository-using-mock';
+import models from './models';
 
 dotenv.config();
 const port = process.env.PORT;
@@ -17,8 +21,14 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.send('Express + Typescript'));
 
-app.post('/meetings', (req, res) => {
-  const { menteeId, mentorId, startTime, endTime } = _.pick(req.body, ['menteeId', 'mentorId', 'startTime', 'endTime']);
+app.post('/meetings', async (req, res) => {
+  const { menteeId, mentorId, startTime, endTime, session } = _.pick(req.body, [
+    'menteeId',
+    'mentorId',
+    'startTime',
+    'endTime',
+    'session',
+  ]);
 
   const meetingParticipants: MeetingParticipantsInterface = MeetingParticipants.load({ menteeId, mentorId });
   const meetingTime: MeetingTimeInterface = MeetingTime.load({ startTime, endTime });
@@ -27,7 +37,23 @@ app.post('/meetings', (req, res) => {
     meetingTime
   );
 
-  return res.send('create a meeting');
+  const meetingProviderForVSee = new MeetingProviderForVSee(
+    process.env.VSEE_API_KEY!,
+    process.env.VSEE_API_SECRET!,
+    process.env.VSEE_API_TOKEN!
+  );
+
+  const meetingRepositoryUsingMock = new MeetingRepositoryUsingMock(models.Meeting);
+
+  const meetingId = meetingRepositoryUsingMock.getNextId();
+
+  const meetingScheduleService = new MeetingScheduleService(meetingProviderForVSee, meetingRepositoryUsingMock);
+
+  await meetingScheduleService.execute(meetingScheduleInput, meetingId, session);
+
+  return res.status(201).send({
+    success: true,
+  });
 });
 
 app.listen(port, () => {
